fcfcdce9acfeb4b7338594363a4a4254b8b9c0c0
Ivan Porto Carrero <ivan@flanders.co.nz>
2009-02-15 23:03:43 +0100

    Fixed the sample to work with the layout

--- IronRubyMvc/Controllers/RubyController.cs
+++ IronRubyMvc/Controllers/RubyController.cs
@@ -31,6 +31,10 @@ namespace IronRubyMvcLibrary.Controllers
         public string ControllerName { get; internal set; }
         public RubyClass RubyType { get; private set; }
 
+//        public static Hash ActionFilters { get; set; }
+//
+//        public static Hash ActionResult { get; set; }
+
         public string ControllerClassName
         {
             get { return Constants.CONTROLLERCLASS_FORMAT.FormattedWith(ControllerName); }
--- IronRubyMvc/Controllers/controller.rb
+++ IronRubyMvc/Controllers/controller.rb
@@ -1,72 +1,44 @@
 ﻿module IronRubyMvc
   
-  module Controllers
+#  module Controllers
+#    
+#    module Filters
+#      
+#      module ClassMethods
+#        
+#        
+#        
+#      end
+#      
+#      def self.included(base)
+#        base.extend(IronRubyMvc::Controllers::Filters::ClassMethods)
+#      end
+#      
+#    end
+#    
+#    module Selectors
+#      
+#      module ClassMethods
+#        
+#        
+#        
+#      end
+#      
+#      def self.included(base)
+#        base.extend(IronRubyMvc::Controllers::Selectors::ClassMethods)
+#      end
+#    end
     
-    module Filters
-      
-      module ClassMethods
-        
-        def before_filter(name, options, &b)
-          filter(name, options.merge(:when => :before), &b)     
-        end
-        
-        def after_filter(name, options, &b)
-          filter(name, options.merge(:when => :after), &b)
-        end
-        
-        def around_filter(name, options, &b)
-          filter(name, options.merge(:when => :around), &b)
-        end
-        
-        def filter(name, options, &b)
-          @action_filters ||= {}
-          options[:action] = b if block_given?
-          options[:action] ||= method(name.to_sym)
-          @action_filters[name.to_sym] = options
-        end
-        
-        def action_filters
-          @action_filters
-        end
-        
-      end
-      
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
-      
-    end
-    
-    module Selectors
-      
-      module ClassMethods
-        
-        def action_selector(name, options, &b)
-          @action_selectors ||= {}
-          options[:action] = b if block_given?
-          options[:action] ||= method(name.to_sym)
-          @action_selectors[name.to_sym]
-        end
-        
-        def action_selectors
-          @action_selectors
-        end
-        
-      end
-      
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
-    end
-    
-  end
+#  end
   
   #module Controllers
   
   class Controller < IronRubyMvcLibrary::Controllers::RubyController
     
-    include IronRubyMvc::Controllers::Filters
-    include IronRubyMvc::Controllers::Selectors    
+    #include IronRubyMvc::Controllers::Filters
+    #include IronRubyMvc::Controllers::Selectors   
+    
+    #cattr_accessor :action_filters, :action_selectors
     
     def fill_view_data
       instance_variables.each { |varname| view_data.Add(varname[1..-1], instance_variable_get(varname.to_sym)) }
@@ -75,6 +47,44 @@
     def return_view(*args)
       fill_view_data
       view args
+    end
+    
+    class << self
+      
+      def before_filter(name, options, &b)
+        filter(name, options.merge(:when => :before), &b)     
+      end
+      
+      def after_filter(name, options, &b)
+        filter(name, options.merge(:when => :after), &b)
+      end
+      
+      def around_filter(name, options, &b)
+        filter(name, options.merge(:when => :around), &b)
+      end
+      
+      def filter(name, options, &b)
+        @@action_filters ||= {}
+        options[:action] = b if block_given?
+        options[:action] ||= method(name.to_sym)
+        @@action_filters[name.to_sym] = options
+      end
+      
+      def action_filters
+        @@action_filters
+      end
+      
+      def action_selector(name, options, &b)
+        @@action_selectors ||= {}
+        options[:action] = b if block_given?
+        options[:action] ||= method(name.to_sym)
+        @@action_selectors[name.to_sym] = options
+      end
+      
+      def action_selectors
+        @@action_selectors
+      end
+      
     end
     
   end
