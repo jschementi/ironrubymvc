14e19c41c4d24b893ec3b3c7a560aabfcff3ba01
Jimmy Schementi <jschementi@gmail.com>
2009-03-13 21:04:28 -0400

    Update MVC to 1.0

---
+++ dependencies/update.bat
@@ -0,0 +1,12 @@
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\IronRuby.dll %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\IronRuby.pdb %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\IronRuby.Libraries.dll %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\IronRuby.Libraries.pdb %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\IronRuby.Libraries.Yaml.dll %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\IronRuby.Libraries.Yaml.pdb %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\Microsoft.Scripting.dll %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\Microsoft.Scripting.pdb %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\Microsoft.Scripting.Core.dll %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\Microsoft.Scripting.Core.pdb %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\Microsoft.Scripting.ExtensionAttribute.dll %~dp0
+copy c:\dev\ironruby\Merlin\Main\Bin\Debug\Microsoft.Scripting.ExtensionAttribute.pdb %~dp0
